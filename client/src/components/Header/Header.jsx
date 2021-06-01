583c3264993050d13ef3701d61ee47d0b4455b1d
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-06-01 09:13:25 +0200

    changed some files and integrated searchBar component into Header component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import SearchBar from '../Search/SearchBar';
 
 function Header() {
+
+    const [searchBar, setSearchBar] = useState(false);
+
     return (
         <>
-
+            <header>
+                <div className="navbar__options">
+                    <div className="navbar__options-left">
+                        <div className="navbar__option">
+                            <FontAwesomeIcon className="icon" icon="bars" />
+                            <p>Menu</p>
+                        </div>
+                        <div className="navbar__option">
+                            <FontAwesomeIcon className="icon" icon="newspaper" />
+                            <p>News</p>
+                        </div>
+                        <div className="navbar__option">
+                            <FontAwesomeIcon className="icon" icon="search" onClick={() => setSearchBar(!searchBar)} />
+                            <p>Search</p>
+                        </div>
+                    </div>
+                    <img className="navbar__logo" src="https://i.ibb.co/DGPL6tk/press-start-logo-grey.png" alt="press-start-logo"></img>
+                    <div className="navbar__options-right">
+                        <div className="navbar__option">
+                            <FontAwesomeIcon className="icon" icon="calendar-alt" />
+                            <p>Calendar</p>
+                        </div>
+                        <div className="navbar__option">
+                            <FontAwesomeIcon className="icon" icon="sign-in-alt" />
+                            <p>Sign In</p>
+                        </div>
+                        <div className="navbar__option">
+                            <FontAwesomeIcon className="icon" icon="shopping-cart" />
+                            <p>Cart</p>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            {searchBar && (<SearchBar />)}
         </>
     )
 }
---
+++ client/src/constants/material-icons.js
@@ -0,0 +1,13 @@
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonIcon from '@material-ui/icons/Person';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+export default {
+    menu: MenuIcon,
+    search: SearchIcon,
+    signIn: PersonIcon,
+    news: LibraryBooksIcon,
+    cart: ShoppingCartIcon
+}
