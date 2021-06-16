c805d7764c4a3dd690d803acbeae0448f40b5038
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-06-16 11:27:32 +0200

    created loading component

---
+++ client/src/components/Loading/Loading.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Loading.scss';
+
+function Loading() {
+    return (
+        <img className="loading__image" src="https://i.ibb.co/V92hF3X/loading.gif" alt="loading" />
+    )
+}
+
+export default Loading;
