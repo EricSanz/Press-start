4f909dbc90869a3957a721175dcbac7863b7ef1c
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-07-02 10:19:33 +0200

    added _colors and improved card

--- client/src/components/List/VideogamesList.jsx
+++ client/src/components/List/VideogamesList.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import Loading from '../Loading/Loading';
 import { loadVideogames } from '../../redux/actions/videogameActions';
 import { bindActionCreators } from 'redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './VideogamesList.scss';
 
 function VideogameList({ videogamesList, dispatch, loading, error }) {
@@ -15,22 +16,69 @@ function VideogameList({ videogamesList, dispatch, loading, error }) {
     }, [videogamesList?.length]);
 
     return (
-        <div className="list__container">
+        <div className={loading ? "list__loading" : "list__container"}>
             {error && <h3 className="noup">Noup</h3>}
             {loading ? <Loading /> : videogamesList && videogamesList.map((videogame) => (
-                // <p>{videogame[0].ps4 ? <p>{videogame.game}</p> : null}</p>
-                <>
+                <div className="videogame__list">
                     {videogame.platforms[0].ps5 ? (
-                        <>
-                            <img src={videogame.platforms[0].platform.edition[0].cover}></img>
-                        </>
+                        <div className="videogame__card">
+                            <p className="videogame__card-title">{videogame.game}</p>
+                            <div className="videogame__favorite--container">
+                                <FontAwesomeIcon icon="heart" />
+                            </div>
+                            <div className="videogame__buy--container">
+                                <FontAwesomeIcon icon="shopping-cart" />
+                            </div>
+                            <img className="videogame__card-image" alt={videogame.game} src={videogame.platforms[0].platform.edition[0].cover}></img>
+                            <div className="videogame__card-price--container">
+                                {videogame.platforms[0].platform.edition[0].sale ? (
+                                    <>
+                                        <div className="videogame__onsale-text">
+                                            <p className="onsale__text">On Sale</p>
+                                        </div>
+                                        <div className="videogame__card-price">
+                                            <p className="price__nosale">{videogame.platforms[0].platform.edition[0].price}€</p>
+                                            <p className="price__onsale">{videogame.platforms[0].platform.edition[0].salePrice}€</p>
+                                        </div>
+                                    </>
+                                ) : (
+                                    <div className="videogame__card-price">
+                                        <p className="price__onsale">{videogame.platforms[0].platform.edition[0].price}€</p>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
                     ) : null}
                     {videogame.platforms[1]?.ps5 ? (
-                        <>
-                            <img src={videogame.platforms[1]?.platform.edition[0].cover}></img>
-                        </>
+                        <div className="videogame__card">
+                            <p className="videogame__card-title">{videogame.game}</p>
+                            <div className="videogame__favorite--container">
+                                <FontAwesomeIcon icon="heart" />
+                            </div>
+                            <div className="videogame__buy--container">
+                                <FontAwesomeIcon icon="shopping-cart" />
+                            </div>
+                            <img className="videogame__card-image" alt={videogame.game} src={videogame.platforms[1]?.platform.edition[0].cover}></img>
+                            <div className="videogame__card-price--container">
+                                {videogame.platforms[1].platform.edition[0].sale ? (
+                                    <>
+                                        <div className="videogame__onsale-text">
+                                            <p className="onsale__text">On Sale</p>
+                                        </div>
+                                        <div className="videogame__card-price">
+                                            <p className="price__nosale">{videogame.platforms[1].platform.edition[0].price}€</p>
+                                            <p className="price__onsale">{videogame.platforms[1].platform.edition[0].salePrice}€</p>
+                                        </div>
+                                    </>
+                                ) : (
+                                    <div className="videogame__card-price">
+                                        <p className="price__onsale" >{videogame.platforms[1].platform.edition[0].price}€</p>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
                     ) : null}
-                </>
+                </div>
                 // <p>{videogame.platforms.ps4 ? <p>{videogame.game}</p> : null}</p>
             ))}
         </div>
