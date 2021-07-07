023bdc066a5c9e1c24c5258bc7346da509fbe14a
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-07-07 10:28:16 +0200

    added post controller to videogames

--- server/src/controllers/videogameController.js
+++ server/src/controllers/videogameController.js
@@ -8,8 +8,32 @@ function videogameController(VideogameModel) {
             ? res.send(errorGettingVideogames) : res.json(videogames)));
     }
 
+    function postEdition({ body }, res) {
+        const videogameId = body.id;
+        VideogameModel.findOne({ id: videogameId }, (errorFindUser, videogame) => {
+            if (videogame) {
+                const findVideogame = videogame.other_editions.some(
+                    (videogame) => String(videogame) === body.videogame,
+                );
+                if (findVideogame) {
+                    const videogameFilter = videogame.other_editions.filter((videogame) => String(videogame) !== body.videogame);
+                    videogame.other_editions = videogameFilter;
+                    videogame.save();
+                    res.send('delete');
+                } else {
+                    videogame.other_editions = [...videogame.other_editions, body.videogame];
+                    videogame.save();
+                    res.json('save');
+                }
+            } else {
+                res.send(errorFindUser);
+            }
+        });
+    }
+
     return {
         getVideogames,
+        postEdition
     };
 }
 
