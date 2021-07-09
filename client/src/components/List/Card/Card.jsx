3cfae9036247099905a52d888af4ee26f547dc35
Eric <eric_sanz_rodriguez@hotmail.com>
2021-07-09 22:08:04 +0200

    created Card file for List videogames

---
+++ client/src/components/List/Card/Card.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+import './Card.scss';
+
+function Card({Games}) {
+    return (
+        <div className="videogame__list">
+            <div className="videogame__card">
+                {Games.game.dual_title ? (
+                    <div className="title__container">
+                        <p className="videogame__card-title">{Games.game.first_title}</p>
+                        <p className="videogame__card-title">{Games.game.second_title}</p>
+                    </div>
+                ) : (
+                    <div className="title__container">
+                        <p className="videogame__card-title">{Games.game.first_title}</p>
+                    </div>
+                )}
+                <div className="videogame__favorite--container">
+                    <FontAwesomeIcon icon="heart"/>
+                </div>
+                <div className="videogame__buy--container">
+                    <FontAwesomeIcon icon="shopping-cart" className={Games.edition.stock ? "green" : "red"} />
+                </div>
+                <Link to={`product/${Games._id}`}>
+                    <img className="videogame__card-image" alt={Games.id} src={Games.edition.cover}></img>
+                </Link>
+                <div className="videogame__card-price--container">
+                    {Games.edition.sale ? (
+                        <>
+                            <div className="videogame__onsale-text">
+                                <p className="onsale__text">On Sale</p>
+                            </div>
+                            <div className="videogame__card-price">
+                                <p className="price__nosale">{Games.edition.price}€</p>
+                                <p className="price__onsale">{Games.edition.salePrice}€</p>
+                            </div>
+                        </>
+                    ) : (
+                        <div className="videogame__card-price">
+                            <p className="price__onsale">{Games.edition.price}€</p>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
