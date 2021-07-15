01578767ef5ff3124e8b122574b72df2adc71a0a
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-07-15 21:05:03 +0200

    implemented edition and platform buttons

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -1,23 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { loadOneVideogame } from '../../redux/actions/videogameActions';
+import { Link } from 'react-router-dom';
+import Loading from '../Loading/Loading';
+import EditionButton from './Edition-Button/Edition-Button';
+import PlatformButton from './Platform-Button/Platform-Button';
 import './Details.scss';
 
-function Details ({dispatch, videogame, match}) {
+function Details ({dispatch, videogame, match, loading}) {
 
     const [id] = useState(match.params.videogameId);
 
     useEffect(() => {
-        if (!videogame || videogame.id !== id) {
+        if (!videogame || videogame._id !== id) {
             dispatch(loadOneVideogame(id))
         }
     }, [])
 
     return (
         <main>
-            {videogame && (
-                <p>{videogame.game.first_title}</p>
-            )}
+            <div className="main__left">
+                {loading ? <Loading/> : videogame && (
+                    <>
+                        <p>{videogame.game.first_title}</p>
+                        <p>{videogame.id}</p>                        
+                        {videogame.other_platforms.length > 0 ? (
+                            <p>Other platforms:</p>
+                        ) : null}
+                        <div className="editions__container">
+                            {videogame.other_platforms.length > 0 && videogame.other_platforms.map((platforms) => (
+                                <PlatformButton platforms={platforms}/>
+                            ))}
+                        </div>
+                        {videogame.other_editions.length > 0 ? (
+                            <p>Other Editions:</p>
+                        ) : null}
+                        <div className="editions__container">
+                            {videogame.other_editions.length > 0 && videogame.other_editions.map((editions) => (
+                                <EditionButton editions={editions}/>
+                            ))}
+                        </div>
+                    </>
+                )}
+            </div>
+            <div className="main__right">
+
+            </div>
         </main>
     )
 };
@@ -25,6 +53,8 @@ function Details ({dispatch, videogame, match}) {
 function mapStateToProps({videogameReducer}) {
     return {
         videogame: videogameReducer.videogame,
+        loading: videogameReducer.loading,
+        error: videogameReducer.error,
     }
 }
 
