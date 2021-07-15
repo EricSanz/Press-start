13073451255eddb7a3902c2b38ee0644fd4dd0da
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-07-15 21:04:16 +0200

    created Platform-Button component and already connected and working properly

---
+++ client/src/components/Details/Platform-Button/Platform-Button.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { loadOneVideogame } from '../../../redux/actions/videogameActions';
+import './Platform-Button.scss';
+
+function PlatformButton({dispatch, platforms}) {
+    return (
+        <div className="platforms__main">
+            <Link to={`${platforms._id}`} style={{ textDecoration: 'none' }} onClick={() => dispatch(loadOneVideogame(platforms._id))}>
+                <div className="platforms">
+                    <p className="platform">{platforms.edition.version}</p>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+function mapStateToProps({videogameReducer}) {
+    return {
+        videogame: videogameReducer.videogame,
+        loading: videogameReducer.loading,
+        error: videogameReducer.error,
+    }
+}
+
+export default connect(mapStateToProps)(PlatformButton);
