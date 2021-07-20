37ad3f1a320444400754a8306249ac26d755854e
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-07-20 20:06:03 +0200

    search component remade

--- client/src/components/Search/SearchBar.jsx
+++ client/src/components/Search/SearchBar.jsx
@@ -5,28 +5,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function SearchBar() {
 
     const [searchBar, setSearchBar] = useState(false);
+    const [searchIcon, setSearchIcon] = useState(false);
+    const [searchInput, setSearchInput] = useState(false);
 
     function handleSearchBar() {
         setSearchBar(!searchBar);
+        setSearchIcon(!searchIcon);
+        setSearchInput(!searchInput);
 
-        const openSearchBar = document.getElementById('search__component');
-        searchBar ? openSearchBar.style.display = 'none' : openSearchBar.style.display = 'inline-flex';
-
-        // const colorSearchIcon = document.getElementById('search');
-
-        // searchBar ? colorSearchIcon.style.color = '#E8A92C' : colorSearchIcon.style.color = '#E8A92C';
+        const openSearchBar = document.getElementById('search');
+        const searchIconPlace = document.getElementById('search__icon');
+        const searchInputDisplay = document.getElementById('search__input');
+        searchBar ? openSearchBar.style.transform = 'translateX(0px)' : openSearchBar.style.transform = 'translateX(330px)';
+        searchIcon ? searchIconPlace.style.transform = 'translateX(0px)' : searchIconPlace.style.transform = 'translateX(-330px)';
+        searchInput ? searchInputDisplay.style.display = 'none' : searchInputDisplay.style.display = 'block';
     }
 
     return (
-        <>
-            <div className="navbar__option" id="search">
-                <FontAwesomeIcon className="icon" icon="search" onClick={() => handleSearchBar()} />
-                <p>Search</p>
-            </div>
-            <div className="search__container" id="search__component">
-                <FontAwesomeIcon className="search__icon" icon="search" />
-                <input type="text" placeholder="Search..." />
-            </div>
-        </>
+        <div className="search__filter" id="search">
+            <FontAwesomeIcon id="search__icon" className="search--icon" icon="search" onClick={() => handleSearchBar()} />
+            <input className="search--input" id="search__input" type="text" placeholder="Search..." />
+        </div>
     )
 }
