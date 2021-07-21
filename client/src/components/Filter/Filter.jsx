e279c6b10db15aa7042fab9a8e4a6839f5d7b638
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-07-21 20:14:28 +0200

    created filter component base scss with open/close button functionality done

---
+++ client/src/components/Filter/Filter.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './Filter.scss';
+import '../List/VideogamesList.scss';
+
+function FilterComponent() {
+
+    const [inputNav, setInputNav] = useState(false);
+
+    function handleInputNav() {
+
+        setInputNav(!inputNav);
+
+        const openInputNav = document.getElementById('filter__options-id');
+        const cardContainer = document.getElementById('list__container-id');
+        const filterIconRight = document.getElementById('filter__icon__right-id');
+        const filterIconLeft = document.getElementById('filter__icon__left-id');
+        const filterButton = document.getElementById('filter__button-id');
+        const filterTagOpen = document.getElementById('filter__tag__open-id');
+        const filterTagClose = document.getElementById('filter__tag__close-id');
+        inputNav ? openInputNav.style.transform = 'translateX(330px)' : openInputNav.style.transform = 'translateX(0px)';
+        inputNav ? cardContainer.style['padding-left'] = '280px' : cardContainer.style['padding-left'] = '0px';
+        inputNav ? filterIconRight.style.display = 'none' : filterIconRight.style.display = 'block';
+        inputNav ? filterIconLeft.style.display = 'block' : filterIconLeft.style.display = 'none';
+        inputNav ? filterButton.style.background = '#161616af' : filterButton.style.background = '#e15b64';
+        inputNav ? filterButton.style.color = '#fff' : filterButton.style.color = '#161616';
+        inputNav ? filterTagOpen.style.display = 'none' : filterTagOpen.style.display = 'block';
+        inputNav ? filterTagClose.style.display = 'block' : filterTagClose.style.display = 'none';
+    }
+
+    return (
+        <>
+            <div className="filter__button" id="filter__button-id" onClick={() => handleInputNav()}>
+                <p className="filter__tag" id="filter__tag__open-id">Filters</p>
+                <p className="filter__tag" id="filter__tag__close-id">Close</p>
+                <FontAwesomeIcon id="filter__icon__right-id" className="filter__icon" icon="angle-double-right" />
+                <FontAwesomeIcon id="filter__icon__left-id" className="filter__icon" icon="angle-double-left" />
+            </div>
+            <div className="filter__options" id="filter__options-id">
+
+            </div>
+        </>
+    )
+}
+
+
+function mapStateToProps({ videogameReducer }) {
+    return {
+        videogamesList: videogameReducer.videogamesList,
+        filteredVideogameList: videogameReducer.filteredVideogameList,
+        loading: videogameReducer.loading,
+        error: videogameReducer.error,
+    }
+}
+
+export default connect(mapStateToProps)(FilterComponent);
