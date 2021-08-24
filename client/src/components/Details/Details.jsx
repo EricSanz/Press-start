10c2190395bb2bbbde48de649f60eeda6cade11b
Eric <eric_sanz_rodriguez@hotmail.com>
2021-08-24 18:03:40 +0200

    favorite option css and general info css added

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { loadOneVideogame } from '../../redux/actions/videogameActions';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Loading from '../Loading/Loading';
 import EditionButton from './Edition-Button/Edition-Button';
 import PlatformButton from './Platform-Button/Platform-Button';
@@ -30,6 +31,9 @@ function Details ({dispatch, videogame, match, loading}) {
                             )}
                             <p className="title--addon">{videogame.edition.version} ({videogame.edition.name} Edition)</p>
                         </div>
+                        <div className="videogame__favorite">
+                            <FontAwesomeIcon className="heart-icon" icon="heart"/>
+                        </div>
                         {videogame.edition.sale ? (
                             <div className="onsale__tag">
                                 <p>On Sale</p>
@@ -48,7 +52,14 @@ function Details ({dispatch, videogame, match, loading}) {
                                     <PlatformButton platforms={platforms}/>
                                     ))}
                             </div>
-                        ) : null}
+                        ) : (
+                            <div className="platforms__container">
+                            <p className="platforms__title">Platforms:</p>
+                            <div className="selected-platform__container">
+                                <p className="selected-platform">{videogame.edition.version}</p>
+                            </div>
+                        </div>
+                        )}
                         {videogame.other_editions.length > 0 ? (
                             <div className="editions__container">
                                 <p className="editions__title">Editions:</p>
@@ -59,50 +70,80 @@ function Details ({dispatch, videogame, match, loading}) {
                                     <EditionButton editions={editions}/>
                                     ))}
                             </div>
-                        ) : null}
+                        ) : (
+                            <div className="editions__container">
+                                <p className="editions__title">Editions:</p>
+                                <div className="selected-edition__container">
+                                    <p className="selected-edition">{videogame.edition.name}</p>
+                                </div>
+                            </div>
+                        )}
                     </>
                 )}
             </div>
             <div className="main__right">
                 {loading ? <Loading /> : videogame && (
                     <>
-                        <div className="rating__global--container">
-                            <p className="rating__global--title">Rating:</p>
-                            <div className="rating__global--stars">
-                                <div className="stars__rating--top" style={{width: "71%"}}><span>★</span>
-                                <span>★</span><span>★</span><span>★</span><span>★</span></div>
-                                <div className="stars__ratring--bottom"><span>★</span><span>★</span>
-                                <span>★</span><span>★</span><span>★</span></div>
-                            </div>
-                            <p className="rating__global--opinions">from X Number of players</p>
-                        </div>
-                        {videogame.edition.sale ? (
-                            <div className="onsale__price--container">
-                                <p className="normal__price">{videogame.edition.price}€</p>
-                                <p className="onsale__price">{videogame.edition.salePrice}€</p>
-                            </div>
-                        ) : (
-                            <div className="normal__price--container">
-                                <p className="normal__price">{videogame.edition.price}€</p>
-                            </div>
-                        )}
-                        {videogame.edition.stock ? (
-                            <>
-                                {videogame.release.released ? (
-                                    <div className="add__cart__container">
-                                        <p>ADD TO CART</p>
+                        <div className="info__container">
+                            <div className="price__cart--option">
+                                {videogame.edition.sale ? (
+                                    <div className="onsale__price--container">
+                                        <p className="normal__price__onsale">{videogame.edition.price}€</p>
+                                        <p className="onsale__price">{videogame.edition.salePrice}€</p>
                                     </div>
                                 ) : (
-                                    <div className="preorder__container">
-                                        <p>PRE-ORDER</p>
+                                    <div className="normal__price--container">
+                                        <p className="normal__price">{videogame.edition.price}€</p>
                                     </div>
                                 )}
-                            </>
-                        ) : (
-                            <div className="sold__out__container">
-                                <p>SOLD OUT</p>
+                                {videogame.edition.stock ? (
+                                    <>
+                                        {videogame.release.released ? (
+                                            <button className="add__cart__container">
+                                                <p>ADD TO CART</p>
+                                            </button>
+                                        ) : (
+                                            <button className="preorder__container">
+                                                <p>PRE-ORDER</p>
+                                            </button>
+                                        )}
+                                    </>
+                                ) : (
+                                    <div className="sold__out__container">
+                                        <p>SOLD OUT</p>
+                                    </div>
+                                )}
                             </div>
-                        )}
+                            <div className="general__info__container">
+                                <div className="developer__genres__rating--info">
+                                    <div className="rating__container">
+                                        <p className="rating__title">Rating:</p>
+                                        <div className="rating__stars">
+                                            <div className="stars__top" style={{width: "71%"}}><span>★</span>
+                                            <span>★</span><span>★</span><span>★</span><span>★</span></div>
+                                            <div className="stars__bottom"><span>★</span><span>★</span>
+                                            <span>★</span><span>★</span><span>★</span></div>
+                                        </div>
+                                        <p className="rating__opinions">from X Number of players</p>
+                                    </div>
+                                    <div className="developer__genres__container">
+                                        <div className="genres__container">
+                                            <p className="genres__title">Genres:</p>
+                                            {videogame.genres.map((genre) => (
+                                                <p className="genres">{genre}</p>
+                                            ))}
+                                        </div>
+                                        <div className="developer__container">
+                                                <p className="developer__title">Developer:</p>
+                                                <p>{videogame.developer}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="pegi__container">
+                                    <img className="pegi" src={videogame.pegi} alt="pegi" />
+                                </div>
+                            </div>
+                        </div>
                     </>
                 )}
             </div>
