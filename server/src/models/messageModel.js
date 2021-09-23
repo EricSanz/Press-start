265a9176150b1966374ea0115d608c0f4d3c2d40
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-09-23 19:57:12 +0200

    created message model

---
+++ server/src/models/messageModel.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require('mongoose');
+
+const messageSchema = new Schema({
+    user: [{ type: Schema.Types.ObjectId, ref: 'user' }],
+    videogame: String,
+    date: { type: Date, default: new Date() },
+    comment: [
+        {
+            title: String,
+            message: String
+        }
+    ],
+    rating: String 
+});
+
+module.exports = model('messages', messageSchema);
