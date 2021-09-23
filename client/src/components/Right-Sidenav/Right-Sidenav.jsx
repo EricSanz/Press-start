62c4eedfded4bbac8e6b5dbd7d27448bd1131853
Eric <eric_sanz_rodriguez@hotmail.com>
2021-09-23 14:46:54 +0200

    right sidenav sign in scss done

--- client/src/components/Right-Sidenav/Right-Sidenav.jsx
+++ client/src/components/Right-Sidenav/Right-Sidenav.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Right-Sidenav.scss';
+import googleLogo from '../../svg/google.svg';
+import facebookLogo from '../../svg/facebook.svg';
 
 function RightSidenav() {
 
     const [togglePassword, setTogglePassword] = useState(false);
+    const [toggleActiveOption, setToggleActiveOption] = useState(false);
 
     function seeHidePassword() {
         const seePassword = document.getElementById('see__password');
@@ -18,16 +21,34 @@ function RightSidenav() {
         togglePassword ? passwordInput.type = 'text' : passwordInput.type = 'password';
     }
 
+    function activeOption() {
+        const signInOption = document.getElementById('signIn__option');
+        const registerOption = document.getElementById('register__option');
+
+        setToggleActiveOption(!toggleActiveOption);
+
+        toggleActiveOption ? signInOption.className = 'active signIn__register--option' : signInOption.className = 'option signIn__register--option';
+        toggleActiveOption ? registerOption.className = 'option signIn__register--option' : registerOption.className = 'active signIn__register--option';
+    }
+
     return (
         <div className="right__sidenav">
             <div className="signIn__register">
-                <button className="active signIn__register--option">Sign In</button>
-                <button className="option signIn__register--option">Register</button>
+                <button id="signIn__option" className="active signIn__register--option" onClick={() => activeOption()}>Sign In</button>
+                <button id="register__option" className="option signIn__register--option" onClick={() => activeOption()}>Register</button>
             </div>
-            <input className="email__input" type="text" placeholder="Email" />
-            <input id="password" className="password__input" type="text" placeholder="Password" />
-            <FontAwesomeIcon id="see__password" className="password__icon not--slashed" icon="eye"  onClick={() => seeHidePassword()}/>
-            <FontAwesomeIcon id="hide__password" className="password__icon slashed" icon="eye-slash"  onClick={() => seeHidePassword()}/>
+            <div className="signIn__menu">
+                <input className="email__input" type="text" placeholder="Email" />
+                <input id="password" className="password__input" type="text" placeholder="Password" />
+                <FontAwesomeIcon id="see__password" className="password__icon not--slashed" icon="eye" onClick={() => seeHidePassword()}/>
+                <FontAwesomeIcon id="hide__password" className="password__icon slashed" icon="eye-slash" onClick={() => seeHidePassword()}/>
+                <button className="signIn__button">Sign In</button>
+                <p className="p__signin">Or sign in easily with:</p>
+                <div className="logos__container">
+                    <img src={googleLogo} alt="google" />
+                    <img src={facebookLogo} alt="facebook" />
+                </div>
+            </div>
         </div>
     )
 }
