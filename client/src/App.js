1eea5811cade82e19054540a57d709bd1cafd741
Eric <eric_sanz_rodriguez@hotmail.com>
2021-09-27 18:05:27 +0200

    imported and routed dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
 import VideogamesList from './components/List/VideogamesList';
 import Details from './components/Details/Details';
+import Dashboard from './components/DashBoard/dashboard';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars, faSearch, faUser, faNewspaper, faShoppingCart, faSignInAlt, faCalendarAlt, faHome, faAngleDoubleRight, faAngleDoubleLeft, faAngleDoubleDown, faAngleDoubleUp, faEnvelopeOpenText, faHeart, faTimes, faCheckCircle, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
@@ -16,6 +17,7 @@ function App() {
         <Route exact path="/" component={Landing} />
         <Route path="/all-games" component={VideogamesList} />
         <Route path="/product/:videogameId" component={Details} />
+        <Route path="/dashboard/:userId" exact component={Dashboard} />
       </Switch>
     </>
   );
