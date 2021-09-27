4b4593ef584deaccc3b7fa9722b48d44adaf6ff2
Eric <eric_sanz_rodriguez@hotmail.com>
2021-09-27 18:08:37 +0200

    implemented useDispatch

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -2,17 +2,21 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { loadVideogames } from '../../redux/actions/videogameActions';
 import Loading from '../Loading/Loading';
+import { useDispatch } from 'react-redux';
 import MainSlider from '../Main-Slider/Main-Slider';
 import CardSlider from '../Card-Slider/Card-Slider';
 import './Landing.scss';
 
-function Landing({ videogamesList, dispatch }) {
+function Landing({ videogamesList }) {
+   
+    const dispatch = useDispatch();
 
     useEffect(() => {
         if (!videogamesList?.length) {
             dispatch(loadVideogames());
         }
-    }, [videogamesList?.length])
+
+    }, [dispatch, videogamesList?.length])
 
 
     return (
