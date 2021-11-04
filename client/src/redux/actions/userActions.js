436977d9e4d6a4139a13e718927c5bed50de5a83
Eric <eric_sanz_rodriguez@hotmail.com>
2021-11-04 13:27:40 +0100

    register and login user actions done, need to deleted commented lines

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -1,9 +1,11 @@
 import './Firebase/firebaseIndex';
 import axios from 'axios';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { GoogleAuthProvider, getAuth, signInWithPopup, createUserWithEmailAndPassword } from 'firebase/auth';
 import actionTypes from './actionTypes';
 
 const userUrl = 'http://localhost:5000/user';
+const userLoginUrl = 'http://localhost:5000/user/login';
+const userRegisterUrl = 'http://localhost:5000/user/register';
 
 export function signInSuccess(user) {
     return {
@@ -63,7 +65,7 @@ export function addUserError(error) {
 export function addUser(userData) {
     return async (dispatch) => {
         try {
-            const { data } = await axios.put(userUrl, userData);
+            const { data } = await axios.put(userRegisterUrl, userData);
             localStorage.user = JSON.stringify({ user: { ...data } });
             dispatch(addUserSucces(data));
         } catch (error) {
@@ -98,3 +100,99 @@ export function signOut() {
     };
 };
 
+export function registerSuccess(user) {
+    return {
+        type: actionTypes.REGISTER_USER,
+        user,
+    };
+};
+
+export function registerError(error) {
+    console.log(error);
+    return {
+        type: actionTypes.REGISTER_USER_ERROR,
+        error,
+    };
+};
+
+// const email = document.getElementById('email').value;
+// const password = document.getElementById('password').value;
+// const userName = document.getElementById('user_Name').value;
+export function registerUser(email, password, displayName) {
+    // const auth = getAuth();
+    return async (dispatch) => {
+        const userData = { email, password, displayName };
+        // localStorage.setItem('user', JSON.stringify({
+        //     email: userData.email,
+        //     password: userData.password,
+        //     displayName: userData.displayName,
+        // }));
+        console.log(userData);
+        // dispatch(signInSuccess(userData));
+        try {
+            // const { data } = axios.post(userUrl, userData);
+            const response = await axios.post(userRegisterUrl, userData);
+            console.log(response);
+            if (response.data.error) {
+                console.log(response.data.error.msg)
+                dispatch(registerError(response.data.error))
+            } else {
+                console.log(response.statusText);
+                localStorage.user = JSON.stringify({ user: { ...response } });
+                console.log(localStorage.user);
+                const { user } = response;
+                dispatch(registerSuccess(user));
+                // dispatch(signInSuccess(response));
+                // dispatch(registerError(error))
+                // console.log(response.data.error.msg);
+            }
+            // console.log(localStorage.user.user);
+            // console.log(data);
+        } catch (error) {
+            console.log(error);
+            // console.log(response.error.msg);
+            dispatch(registerError(error))
+        }
+    }
+}
+
+export function loginUser(email, password) {
+    return async (dispatch) => {
+        const userData = {email, password};
+        console.log(userData);
+        try {
+            const response = await axios.post(userLoginUrl, userData);
+            console.log(response);
+            if (response.data.error) {
+                dispatch(signInError(response.data.error));
+            } else {
+                localStorage.user = JSON.stringify({ user: {...response } });
+                const { user } = response;
+                dispatch(signInSuccess(user));
+            }
+        } catch (error) {
+            dispatch(signInError(error))
+        }
+    }
+}
+
+
+
+// export function registerUser() {
+//     const auth = getAuth();
+//     const email = document.getElementById('email').value;
+//     const password = document.getElementById('password').value;
+//     const userName = document.getElementById('user_Name').value;
+//     return async (dispatch) => {
+//         try {
+//             createUserWithEmailAndPassword(auth, email, password, userName)
+//             .then((userCredential) => {
+//                 const user = userCredential.user;
+//                 console.log(user)
+//                 dispatch(registerSuccess(user));
+//             } )
+//         } catch (error) {
+//             dispatch(registerError(error))
+//         }
+//     }
+// }
