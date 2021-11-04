3d43c65ed6ce68b68609565346f28f192ed2d967
Eric <eric_sanz_rodriguez@hotmail.com>
2021-11-04 13:26:30 +0100

    added cases for register, register error and add user error

--- client/src/redux/reducers/userReducer.js
+++ client/src/redux/reducers/userReducer.js
@@ -37,6 +37,27 @@ export default function userReducer(state = {}, action) {
                 user: action.newUser
             };
             break;
+        case actionTypes.ADD_USER_ERROR:
+            newState = {
+                ...state,
+                error: action.error
+            }
+            break;
+        case actionTypes.REGISTER_USER:
+            newState = {
+                ...state,
+                user: action.user,
+                error: null,
+                isLogged: true
+            }
+            break;
+        case actionTypes.REGISTER_USER_ERROR:
+            newState = {
+                ...state,
+                user: null,
+                error: action.error
+            }
+            break;
         default:
             newState = state;
         break;
