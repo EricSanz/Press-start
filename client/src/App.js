0be2aeebfb48f0cb68260563b716759078403633
Eric <eric_sanz_rodriguez@hotmail.com>
2021-11-04 12:17:53 +0100

    added new routes for login and register

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Details from './components/Details/Details';
 import Dashboard from './components/DashBoard/dashboard';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars, faSearch, faUser, faNewspaper, faShoppingCart, faSignInAlt, faCalendarAlt, faHome, faAngleDoubleRight, faAngleDoubleLeft, faAngleDoubleDown, faAngleDoubleUp, faEnvelopeOpenText, faHeart, faTimes, faCheckCircle, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import Login from './components/LogIn/Login';
+import Register from './components/Register/Register';
 
 library.add(faBars, faSearch, faUser, faNewspaper, faShoppingCart, faSignInAlt, faCalendarAlt, faHome, faAngleDoubleRight, faAngleDoubleLeft, faAngleDoubleDown, faAngleDoubleUp, faEnvelopeOpenText, faHeart, faTimes, faCheckCircle, faEye, faEyeSlash)
 
@@ -18,6 +20,8 @@ function App() {
         <Route path="/all-games" component={VideogamesList} />
         <Route path="/product/:videogameId" component={Details} />
         <Route path="/dashboard/:userId" exact component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
       </Switch>
     </>
   );
