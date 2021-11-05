f7151a5229bfc907b6fa1a12fec2075debcd6924
Eric <eric_sanz_rodriguez@hotmail.com>
2021-11-05 13:28:38 +0100

    starting profile page

--- client/src/components/DashBoard/dashboard.jsx
+++ client/src/components/DashBoard/dashboard.jsx
@@ -1,25 +1,37 @@
 import React, {useState, useEffect} from 'react';
 import { connect, useDispatch } from 'react-redux';
+import './Dashboard.scss';
 
-function UserProfile() {
+function UserProfile(user) {
     const userLocalStorage = JSON.parse(window.localStorage.getItem('user'));
+    const activeUser = userLocalStorage.user.data;
     // console.log(userLocalStorage.user.data.uid);
     const dispatch = useDispatch()
+    console.log(user)
+    console.log(activeUser);
 
     // const [userId] = useState(userLocalStorage.user.data.uid);
 
 
     return (
-        <>
+        <div className="profile__container">
+            <div className="profile">
+                <div className="profile__left__container">
 
-            <p>HEEEY</p>
-        </>
+                </div>
+                <div className="profile__right__container">
+
+                </div>
+            </div>
+            
+        </div>
     )
 }
 
 function mapStateToProps({ userReducer }) {
     return {
-        user: userReducer.user
+        user: userReducer.user,
+        isLogged: userReducer.isLogged,
     }
 }
 
