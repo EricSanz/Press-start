4c0ee60c1c3868186509bfb4b210d489e0ac3582
Eric <eric_sanz_rodriguez@hotmail.com>
2021-11-12 14:24:17 +0100

    left part first look

--- client/src/components/DashBoard/dashboard.jsx
+++ client/src/components/DashBoard/dashboard.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { connect, useDispatch } from 'react-redux';
 import './Dashboard.scss';
 
-function UserProfile(user) {
+function UserProfile({user}) {
     const userLocalStorage = JSON.parse(window.localStorage.getItem('user'));
     const activeUser = userLocalStorage.user.data;
     // console.log(userLocalStorage.user.data.uid);
@@ -17,13 +17,18 @@ function UserProfile(user) {
         <div className="profile__container">
             <div className="profile">
                 <div className="profile__left__container">
-
+                    <div className="profile__image"></div>
+                    <p className="full__name">Full Name</p>
+                    <p className="alias">({user.displayName})</p>
+                    <button className="favorites__btn">Favorites</button>
+                    <button className="comments__btn">Comments</button>
+                    <button className="personal__info__btn">Personal Information</button>
+                    <button className="useful__info__btn">Useful Information</button>
+                    <button className="logout__btn">Log out</button>
                 </div>
                 <div className="profile__right__container">
-
                 </div>
             </div>
-            
         </div>
     )
 }
