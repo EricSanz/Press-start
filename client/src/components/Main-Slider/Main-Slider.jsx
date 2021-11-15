e9a60b55b2bbddeac576ecb72823d6e64d580480
Eric <eric_sanz_rodriguez@hotmail.com>
2021-11-15 21:59:44 +0100

    implemented information from sorted by date and refactured Main-Slider

--- client/src/components/Main-Slider/Main-Slider.jsx
+++ client/src/components/Main-Slider/Main-Slider.jsx
@@ -1,35 +1,25 @@
 import React from 'react';
 import './Main-Slider.scss';
 
-function MainSlider() {
+function MainSlider({ videogames }) {
     return (
         <section className="main-slider">
             <div className="main-slider__gallery">
                 <div className="gallery">
-                    <div className="gallery__slide gallery__slide--1">First slide</div>
-                    <div className="gallery__slide gallery__slide--2">Second slide</div>
-                    <div className="gallery__slide gallery__slide--3">Third slide</div>
-                    <div className="gallery__slide gallery__slide--4">Fourth slide</div>
-                    <div className="gallery__slide gallery__slide--5">Fifth slide</div>
+                    {videogames.map((mainVideogamesSlider) => (
+                        <div className="gallery__slide">
+                            <img className="gallery__image" src={mainVideogamesSlider.card} alt={mainVideogamesSlider.id} />
+                        </div>
+                    ))}
                 </div>
             </div>
             <div className="main-slider__small-images">
                 <div className="small__image-container">
-                    <div className="small__image-1 small__slide--1">
-                        <p className="title">Immortals Fenyx Rising</p>
-                    </div>
-                    <div className="small__image-2 small__slide--2">
-                        <p className="title">Far Cry 6</p>
-                    </div>
-                    <div className="small__image-3 small__slide--3" >
-                        <p className="title">Ghost of Tsushima</p>
-                    </div>
-                    <div className="small__image-4 small__slide--4">
-                        <p className="title">Resident Evil Village</p>
-                    </div>
-                    <div className="small__image-5 small__slide--5">
-                        <p className="title">No Man's Sky</p>
-                    </div>
+                    {videogames.map((mainVideogameMiniSlider) => (
+                        <div className={`small__image small__slide--${mainVideogameMiniSlider.length + 1}`} style={{backgroundImage: `url(${mainVideogameMiniSlider.card})`}}>
+                            <p className="title">{mainVideogameMiniSlider.game.first_title}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
