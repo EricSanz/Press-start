38b779e3882c01cde7a4029604cdd5d52cb2c92a
Eric <eric_sanz_rodriguez@hotmail.com>
2021-11-18 15:32:16 +0100

    added logic to new function for new property to be added correctly

--- server/src/controllers/videogameController.js
+++ server/src/controllers/videogameController.js
@@ -36,34 +36,61 @@ function videogameController(VideogameModel) {
         });
     }
 
-    function putPlatform({ body }, res) {
+    function putPlatformsAvailable({body}, res) {
         const videogameId = body.id;
+        const ps4 = body.ps4Platform; 
+        const ps5 = body.ps5Platform; 
+        const xbox = body.xboxPlatform; 
+        const nintendo = body.switchPlatform; 
+        const pc = body.pcPlatform;
+
         VideogameModel.findOne({ id: videogameId }, (errorFindUser, videogame) => {
             if (videogame) {
-                const findVideogame = videogame.other_platforms.some(
-                    (videogame) => String(videogame) === body.videogame,
-                );
-                if (findVideogame) {
-                    const videogameFilter = videogame.other_platforms.filter((videogame) => String(videogame) !== body.videogame);
-                    videogame.other_platforms = videogameFilter;
-                    videogame.save();
-                    res.send('deleted');
-                } else {
-                    videogame.other_platforms = [...videogame.other_platforms, body.videogame];
-                    videogame.save();
-                    res.json('saved');
-                }
+
+                ps4 !== "" ? videogame.available_platforms_logos.push(ps4) : null;
+                ps5 !== "" ? videogame.available_platforms_logos.push(ps5) : null;
+                xbox !== "" ? videogame.available_platforms_logos.push(xbox) : null;
+                nintendo !== "" ? videogame.available_platforms_logos.push(nintendo) : null;
+                pc !== "" ? videogame.available_platforms_logos.push(pc) : null;
+
+                videogame.available_platforms_logos = [... videogame.available_platforms_logos];
+                videogame.save();
+                res.json('saved');
             } else {
                 res.send(errorFindUser);
             }
         });
     }
 
+    // function putPlatform({ body }, res) {
+    //     const videogameId = body.id;
+    //     VideogameModel.findOne({ id: videogameId }, (errorFindUser, videogame) => {
+    //         if (videogame) {
+    //             const findVideogame = videogame.other_platforms.some(
+    //                 (videogame) => String(videogame) === body.videogame,
+    //             );
+    //             if (findVideogame) {
+    //                 const videogameFilter = videogame.other_platforms.filter((videogame) => String(videogame) !== body.videogame);
+    //                 videogame.other_platforms = videogameFilter;
+    //                 videogame.save();
+    //                 res.send('deleted');
+    //             } else {
+    //                 videogame.other_platforms = [...videogame.other_platforms, body.videogame];
+    //                 videogame.save();
+    //                 res.json('saved');
+    //             }
+    //         } else {
+    //             res.send(errorFindUser);
+    //         }
+    //     });
+    // }
+
     return {
         getVideogames,
         getOneVideogame,
         postEdition,
-        putPlatform
+        // putPlatform,
+        putPlatformsAvailable,
     };
 }
 
