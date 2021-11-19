b7826961e986ad820ebe45ffd2f020c47ff9a737
Eric <eric_sanz_rodriguez@hotmail.com>
2021-11-19 20:06:00 +0100

    Card slider improved and design done

--- client/src/components/Card-Slider/Card-Slider.jsx
+++ client/src/components/Card-Slider/Card-Slider.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import videogames from './card-mockup';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Card-Slider.scss';
 
 function CardSlider({ cards }) {
@@ -8,7 +8,6 @@ function CardSlider({ cards }) {
             {cards.edition.sale ? (
                 <div className="rank">
                     Sale
-                    {/* <p className="sale">Sale</p> */}
                 </div>
             ) : null}
             <div className="front">
@@ -17,76 +16,151 @@ function CardSlider({ cards }) {
                     <>
                         {cards.game.first_title.length > 15 ? (
                             <>
-                                {/* <h3 className="name__first__second__long">{cards.game.first_title}&nbsp;<span>{cards.game.second_title}</span></h3> */}
-                                <h3 className="name__first__second__long">{cards.game.first_title}</h3>
-                                <h3 className="name__second__long">{cards.game.second_title}</h3>
+                                {cards.edition.sale ? (
+                                    <>
+                                        <h3 className="name__first__second__long__sale">{cards.game.first_title}</h3>
+                                        <h3 className="name__second__long__sale">{cards.game.second_title}</h3>
+                                    </>
+                                ) : (
+                                    <>
+                                        <h3 className="name__first__second__long">{cards.game.first_title}</h3>
+                                        <h3 className="name__second__long">{cards.game.second_title}</h3>
+                                    </>
+                                )}
                             </>
                         ) : (
                             <>
-                            {/* <h3 className="name__first__second">{cards.game.first_title}&nbsp;<span>{cards.game.second_title}</span></h3> */}
-                            <h3 className="name__first__second">{cards.game.first_title}</h3>
-                            <h3 className="name__second">{cards.game.second_title}</h3>
+                                {cards.edition.sale ? (
+                                    <>
+                                        <h3 className="name__first__second__sale">{cards.game.first_title}</h3>
+                                        <h3 className="name__second__sale">{cards.game.second_title}</h3>
+                                    </>
+                                ) : (
+                                    <>
+                                        <h3 className="name__first__second">{cards.game.first_title}</h3>
+                                        <h3 className="name__second">{cards.game.second_title}</h3>
+                                    </>
+
+                                )}
                             </>
                         ) }
-                        {/* <h3 className="name__second">{cards.game.second_title}</h3> */}
                     </>
                 ) : (
-                    <h3 className="name__first">{cards.game.first_title}</h3>
-                    // <>
-                    //     {cards.game.first_title.length > 15 ? (
-                    //         <h3 className="name__first__long">{cards.game.first_title}</h3>
-                    //     ) : (
-                    //         <h3 className="name__first">{cards.game.first_title}</h3>
-                    //     )}
-                    // </>
+                    <>
+                        {cards.game.first_title.length > 15 ? (
+                            <>
+                                {cards.edition.sale ? (
+                                    <h3 className="name__first__long__sale">{cards.game.first_title}</h3>
+                                ) : (
+                                    <h3 className="name__first__long">{cards.game.first_title}</h3>
+                                )}
+                            </>
+                        ) : (
+                            <>
+                                {cards.edition.sale ? (
+                                    <h3 className="name__first__sale">{cards.game.first_title}</h3>
+                                ) : (
+                                    <h3 className="name__first">{cards.game.first_title}</h3>
+                                )}
+                            </>
+                        )}
+                    </>
                 )}
-                <div className="stats">
+                <div className="information">
                     {cards.edition.sale ? (
                         <p className="price">{cards.edition.salePrice}€</p>
                     ) : (
                         <p className="price">{cards.edition.price}€</p>
                     )}
-                    <div className="streamers">
-                        {cards.available_platforms_logos?.length > 0 && cards.available_platforms_logos.map((logos) => (
-                            <img src={logos} alt={logos} />
-                        ))}
-                    </div>
+                    {cards.available_platforms_logos?.length > 4 && (
+                        <div className="platforms--5">
+                            {cards.available_platforms_logos.map((logos) => (
+                                <img src={logos} alt={logos} />
+                            ))}
+                        </div>
+                    )}
+                    {cards.available_platforms_logos?.length === 4 && (
+                        <div className="platforms--4">
+                            {cards.available_platforms_logos.map((logos) => (
+                                <img src={logos} alt={logos} />
+                            ))}
+                        </div>
+                    )}
+                    {cards.available_platforms_logos?.length === 3 && (
+                        <div className="platforms--3">
+                            {cards.available_platforms_logos.map((logos) => (
+                                <img src={logos} alt={logos} />
+                            ))}
+                        </div>
+                    )}
+                    {cards.available_platforms_logos?.length === 2 && (
+                        <div className="platforms--2">
+                            {cards.available_platforms_logos.map((logos) => (
+                                <img src={logos} alt={logos} />
+                            ))}
+                        </div>
+                    )}
+                    {cards.available_platforms_logos?.length === 1 && (
+                        <div className="platforms--1">
+                            {cards.available_platforms_logos.map((logos) => (
+                                <img src={logos} alt={logos} />
+                            ))}
+                        </div>
+                    )}
                 </div>
             </div>
             <div className="back">
-                <div className="streamer-info">
-                    {cards.edition.sale ? (
-                        <>
-                            <p className="game-stat">{cards.edition.price}€<span>{cards.edition.salePrice}€</span></p>
-                            <p className="game-stat">On sale <span>{cards.edition.stock ? ('In stock') : 'Out of stock'}</span></p>
-                        </>
-                    ) : (
-                        <>
-                            <p className="game-stat">{cards.edition.price}€</p>
-                            <p className="game-stat">{cards.edition.stock ? ('In stock') : ('Out of stock')}</p>
-                        </>
+                {cards.edition.sale === true ? (
+                    <div className="prices__info-sale">
+                        <p className="prices">{cards.edition.salePrice}€<span>{cards.edition.price}€</span></p>
+                        <FontAwesomeIcon className="add-favorite" icon="heart"/>
+                        {/* <p className="prices">{cards.edition.price}€</p> */}
+                    </div>
+                ) : (
+                    <div className="prices__info">
+                        <p className="prices">{cards.edition.price}€</p>
+                        <FontAwesomeIcon className="add-favorite" icon="heart"/>
+                    </div>
+                )}
+                <div className="platforms">
+                    <p>Available in</p>
+                    {cards.available_platforms_logos?.length > 4 && (
+                        <div className="platforms--5__back">
+                            {cards.available_platforms_logos.map((logos) => (
+                                <img src={logos} alt={logos} />
+                            ))}
+                        </div>
                     )}
-                    {/* <p className="game-stat">{cards.edition.price}€</p>
-                    <p className="game-stat">25.8k<span>Streams</span></p> */}
+                    {cards.available_platforms_logos?.length === 4 && (
+                        <div className="platforms--4__back">
+                            {cards.available_platforms_logos.map((logos) => (
+                                <img src={logos} alt={logos} />
+                            ))}
+                        </div>
+                    )}
+                    {cards.available_platforms_logos?.length === 3 && (
+                        <div className="platforms--3__back">
+                            {cards.available_platforms_logos.map((logos) => (
+                                <img src={logos} alt={logos} />
+                            ))}
+                        </div>
+                    )}
+                    {cards.available_platforms_logos?.length === 2 && (
+                        <div className="platforms--2__back">
+                            {cards.available_platforms_logos.map((logos) => (
+                                <img src={logos} alt={logos} />
+                            ))}
+                        </div>
+                    )}
+                    {cards.available_platforms_logos?.length === 1 && (
+                        <div className="platforms--1__back">
+                            {cards.available_platforms_logos.map((logos) => (
+                                <img src={logos} alt={logos} />
+                            ))}
+                        </div>
+                    )}
                 </div>
                 <button className="btn">See more details</button>
-                <div className="streamers">
-                    <div className="streamer">
-                        <div className="icon"><img src={cards.edition.card} alt="" /></div>
-                        <p className="name">Gamer 1</p>
-                        <p className="number">36.1k</p>
-                    </div>
-                    <div className="streamer">
-                        <div className="icon"><img src={cards.edition.card} alt="" /></div>
-                        <p className="name">Gamer 2</p>
-                        <p className="number">35.2k</p>
-                    </div>
-                    <div className="streamer">
-                        <div className="icon"><img src={cards.edition.card} alt="" /></div>
-                        <p className="name">Gamer 3</p>
-                        <p className="number">30.5k</p>
-                    </div>
-                </div>
             </div>
             <div className="background">
 
