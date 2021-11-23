5587293e3489b42965d62aaf63072380571c0d51
Eric <eric_sanz_rodriguez@hotmail.com>
2021-11-23 20:56:40 +0100

    added movement to right and left to the card slider

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,15 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { connect } from 'react-redux';
 import { loadVideogames, sortVideogamesByDate, sortCardVideogames } from '../../redux/actions/videogameActions';
 import Loading from '../Loading/Loading';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import MainSlider from '../Main-Slider/Main-Slider';
 import CardSlider from '../Card-Slider/Card-Slider';
 import './Landing.scss';
 
-function Landing({ videogamesList, sortedByDate, cardVideogames }) {
+function Landing({ videogamesList, sortedByDate, cardVideogames, user, isLogged }) {
    
     const dispatch = useDispatch();
+    console.log(user);
+
+    let scrollTranslation = 0;
+
+    const cardsContainerID = document.getElementById('videogame__cards-id');
 
     useEffect(() => {
         if (!videogamesList?.length) {
@@ -24,28 +31,61 @@ function Landing({ videogamesList, sortedByDate, cardVideogames }) {
             dispatch(sortCardVideogames(videogamesList));
         }
 
-    }, [dispatch, videogamesList, sortedByDate, cardVideogames])
+    }, [dispatch, videogamesList, sortedByDate, cardVideogames]);
 
+    function moveToLeft() {
+
+        if (scrollTranslation > -0.01) {
+            scrollTranslation += 0;
+        } else {
+            scrollTranslation += 14.5;
+            cardsContainerID.style.transform = `translateX(${scrollTranslation}%)`;
+        }
+    }
+    
+    function moveToRight() {
+        
+        if (scrollTranslation < -114) {
+            scrollTranslation += 0;
+        } else {
+            scrollTranslation -= 14.3;
+            cardsContainerID.style.transform = `translateX(${scrollTranslation}%)`;
+        }
+    }
 
     return (
         <div className="body">
             {sortedByDate?.length > 1 && (
                 <MainSlider videogames={sortedByDate}/>
             )}
-            <div className="videogame__card__slider">
-                {cardVideogames?.length > 0 && cardVideogames.map((videogameCards) => (
-                    <CardSlider cards={videogameCards}/>
-                ))}
+            <div className="card__slider--container">
+                <p className="title__cards">All Videogames</p>
+                <div className="go__left" id="go__left-id" onClick={() => moveToLeft()}>
+                    <FontAwesomeIcon className="icon__left" icon="angle-double-left"/>
+                </div>
+                <div className="videogame__card__slider" id="videogame__cards-id">
+                    {cardVideogames?.length > 0 && cardVideogames.map((videogameCards) => (
+                        <CardSlider cards={videogameCards} loggedUser={user}/>
+                    ))}
+                </div>
+                <div className="go__right" id="go__right-id" onClick={() => moveToRight()}>
+                    <FontAwesomeIcon className="icon__right" icon="angle-double-right"/>
+                </div>
+                <Link className="see__all-link" to={"/all-games"}>
+                    <p>See all <span><FontAwesomeIcon icon="arrow-right" /></span></p>
+                </Link>
             </div>
         </div>
     )
 }
 
-function mapStateToProps({ videogameReducer }) {
+function mapStateToProps({ videogameReducer, userReducer }) {
     return {
         videogamesList: videogameReducer.videogamesList,
         sortedByDate: videogameReducer.sortedByDate,
         cardVideogames: videogameReducer.cardVideogames,
+        user: userReducer.user,
+        isLogged: userReducer.isLogged,
         loading: videogameReducer.loading,
         error: videogameReducer.error,
     }
