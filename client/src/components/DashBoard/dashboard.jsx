e5f7b988c1be088d3fecdc8aa59da6f16104f6e2
Eric <eric_sanz_rodriguez@hotmail.com>
2021-11-23 20:59:34 +0100

    added getuser for reload page problem

--- client/src/components/DashBoard/dashboard.jsx
+++ client/src/components/DashBoard/dashboard.jsx
@@ -1,17 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import { connect, useDispatch } from 'react-redux';
+import { getUser } from '../../redux/actions/userActions';
 import './Dashboard.scss';
 
-function UserProfile({user}) {
+function UserProfile({user, match, activeUser}) {
     const userLocalStorage = JSON.parse(window.localStorage.getItem('user'));
-    const activeUser = userLocalStorage.user.data;
+    const localStorageUser = userLocalStorage.user.data;
     // console.log(userLocalStorage.user.data.uid);
     const dispatch = useDispatch()
     console.log(user)
-    console.log(activeUser);
+    console.log(localStorageUser);
 
-    // const [userId] = useState(userLocalStorage.user.data.uid);
+    const [uid] = useState(match.params.userId);
 
+
+    useEffect(() => {
+
+        if(!user || uid !== localStorageUser.uid ) {
+            dispatch(getUser(localStorageUser.uid));
+        }
+
+    }, [user, dispatch, activeUser, localStorageUser, uid])
 
     return (
         <div className="profile__container">
@@ -19,7 +28,7 @@ function UserProfile({user}) {
                 <div className="profile__left__container">
                     <div className="profile__image"></div>
                     <p className="full__name">Full Name</p>
-                    <p className="alias">({user.displayName})</p>
+                    <p className="alias">({user?.displayName})</p>
                     <button className="favorites__btn">Favorites</button>
                     <button className="comments__btn">Comments</button>
                     <button className="personal__info__btn">Personal Information</button>
@@ -27,6 +36,9 @@ function UserProfile({user}) {
                     <button className="logout__btn">Log out</button>
                 </div>
                 <div className="profile__right__container">
+                    {user?.favorites?.length > 0 && user.favorites.map((videogame) => (
+                        <p>{videogame.game.first_title}</p>
+                    ))}
                 </div>
             </div>
         </div>
@@ -36,6 +48,7 @@ function UserProfile({user}) {
 function mapStateToProps({ userReducer }) {
     return {
         user: userReducer.user,
+        // activeUser: userReducer.activeUser,
         isLogged: userReducer.isLogged,
     }
 }
