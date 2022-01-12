3c95781d02c4d8bc3cc5747ea3383090e673bda9
Eric <eric_sanz_rodriguez@hotmail.com>
2022-01-12 16:04:56 +0100

    updated info for other components

--- client/src/components/DashBoard/UserFavorites/UserFavorites.jsx
+++ client/src/components/DashBoard/UserFavorites/UserFavorites.jsx
@@ -1,103 +1,104 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { connect, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getUser } from '../../../redux/actions/userActions';
+import CardFavorite from './CardFavorites/CardFavorites';
 import './UserFavorites.scss';
 
-function UserFavorites({favorites}) {
+function UserFavorites({ favoritesGamesID, user, match}) {
+
+    let userLocalStorage = JSON.parse(window.localStorage.getItem('user'));
+    const localStorageUser = userLocalStorage?.user;
+    const localStorageUserData = userLocalStorage?.user?.data;
+
+    const dispatch = useDispatch();
+    
+    let googleUser = false;
+    let emailUser = false;
+    let numberOfFavorites = user?.favorites?.length;
+
+    const [googleUserState, setGoogleUserState] = useState(false);
+    const [uid] = useState(match.params.userId);
+    const [favoritesUpdated, setFavoritesUpdated] = useState(false);
+
+    const userId = user?.uid;
+
+    useEffect(() => {
+
+        if((!user || uid !== localStorageUserData?.uid) && localStorageUserData !== undefined) {
+            dispatch(getUser(localStorageUserData?.uid));
+        }
+        if((!user || uid !== localStorageUser?.uid) && localStorageUser !== undefined) {
+            dispatch(getUser(localStorageUser?.uid));
+        }
+
+        if (!user && localStorageUser) {
+            dispatch(getUser(localStorageUser?.uid));
+        }
+        
+        if(!user && userLocalStorage && googleUserState) {
+            dispatch(getUser(localStorageUser?.uid));
+        }
+
+        if (localStorageUser === undefined && !googleUserState) {
+            dispatch(getUser(user?.uid));
+            setGoogleUserState(!googleUserState);
+        }
+
+        if (numberOfFavorites !== user?.favorites?.length) {
+            dispatch(getUser(user.uid));
+            localStorage.user = JSON.stringify({ user: user });
+            setGoogleUserState(!googleUserState);
+        }
+
+    }, [user, user?.favorites?.length, numberOfFavorites, localStorageUserData, localStorageUser, setFavoritesUpdated, favoritesUpdated, dispatch, favoritesGamesID?.length, userLocalStorage, uid, googleUserState, userId, googleUser, emailUser]);
 
     return (
-        <div className='favorite__card__profile' key={favorites.id}>
-            <img className='favorite__image' src={favorites.edition.cover} alt="" />
-            <div className='favorite__name__edition--container'>
-                {favorites.game.dual_title ? (
-                    <>
-                        <p className='name__title'>{favorites.game.first_title}</p>
-                        <p className='name__title'>{favorites.game.second_title}</p>
-                        <p>{favorites.edition.name} Edition</p>
-                        {favorites.edition.sale ? (
-                            <div className="onsale__price--container">
-                                <p className="onsale__price">{favorites.edition.salePrice}€</p>
-                                <p className="normal__price__onsale">{favorites.edition.price}€</p>
-                            </div>
-                        ) : (
-                            <div className="normal__price--container">
-                                <p className="normal__price">{favorites.edition.price}€</p>
-                            </div>
-                        )}
-                    </>
-                ) : (
-                    <>
-                        <p className='name__title'>{favorites.game.first_title}</p>
-                        <p>{favorites.edition.name} Edition</p>
-                        {favorites.edition.sale ? (
-                            <div className="onsale__price--container">
-                                <p className="normal__price__onsale">{favorites.edition.price}€</p>
-                                <p className="onsale__price">{favorites.edition.salePrice}€</p>
-                            </div>
-                        ) : (
-                            <div className="normal__price--container">
-                                <p className="normal__price">{favorites.edition.price}€</p>
-                            </div>
-                        )}
-                    </>
-                )}
-            </div>
-            <div className="general__info__container">
-                <div className="developer__genres__rating--info">
-                    <div className="rating__container">
-                        <p className="rating__title">Rating:</p>
-                        <div className="rating__stars">
-                            <div className="stars__top" style={{width: "71%"}}><span>★</span>
-                            <span>★</span><span>★</span><span>★</span><span>★</span></div>
-                            <div className="stars__bottom"><span>★</span><span>★</span>
-                            <span>★</span><span>★</span><span>★</span></div>
-                        </div>
-                        <p className="rating__opinions">from X Number of players</p>
-                    </div>
-                    <div className="developer__genres__container">
-                        <div className="genres__container">
-                            <p className="genres__title">Genres:</p>
-                            {favorites?.genres?.map((genre) => (
-                                <>
-                                    {favorites?.genres?.indexOf(genre) < 1 ? (
-                                        <p className="genres">{genre}</p>
-                                    ) : (
-                                        <p className="genres"><span>&nbsp;/	&nbsp;&nbsp;</span>{genre}</p>
-                                    )}
-                                </>
-                            ))}
-                        </div>
-                        <div className="developer__container">
-                                <p className="developer__title">Developer:</p>
-                                <p>{favorites?.developer}</p>
-                        </div>
-                    </div>
+        <div className="profile__container">
+            <div className="profile">
+                <div className="profile__left__container">
+                    <img src={user?.photoURL} alt=""/>
+                    <p className="full__name">Welcome &nbsp;<span className="alias">{user?.displayName}!</span></p>
+                    <Link to={`/dashboard/${userId}`}>
+                    <button className="section__btn" id="0" >Personal Information</button>
+                    </Link>
+                    <button className="section__btn btn--active" id="1">Favorites</button>
+                    <Link to={`/dashboard/${userId}`}>
+                    <button className="section__btn" id="2">Comments</button>
+                    </Link>
+                    <a href={`/dashboard/${userId}/useful-information`}>
+                        <button className="section__btn" id="3">Useful Information</button>
+                    </a>
+                    <button className="logout__btn">Log out</button>
                 </div>
-                <div className="pegi__container">
-                    <img className="pegi" src={favorites?.pegi} alt="pegi" />
+                <div className="profile__right__container">
+                        <div className='section' id="favoritesSectionID">
+                            <div className='title__container'>
+                                <h1 className='title'>My favorites</h1>
+                                <div className='heart__container'>
+                                    <FontAwesomeIcon className='heart__count' icon="heart"/>
+                                    <p className='favorites__count'>{user?.favorites?.length}</p>
+                                </div>
+                            </div>
+                            <div className='user__favorites--container'>
+                                {user?.favorites.length > 0 && user?.favorites.map((favorites) => (
+                                    <CardFavorite key={favorites.id} favoritesCard={favorites} loggedUser={user}/>
+                                ))}
+                            </div>
+                        </div>
                 </div>
-            </div>
-            <div className='buttons--container'>
-                {favorites.edition.stock ? (
-                    <>
-                        {favorites.release.released ? (
-                            <button>
-                                ADD TO CART
-                            </button>
-                        ) : (
-                            <button className="preorder">
-                                PRE-ORDER
-                            </button>
-                        )}
-                    </>
-                ) : (
-                    <button className="sold__out">
-                        SOLD OUT
-                    </button>
-                )}
-                {/* <button>ADD TO CART</button> */}
-                <a href={`/product/${favorites._id}`}>DETAILS</a>
             </div>
         </div>
     )
 }
 
-export default UserFavorites;
+function mapStateToProps({ userReducer }) {
+    return {
+        user: userReducer.user,
+        isLogged: userReducer.isLogged,
+        favoritesGamesID: userReducer.favoritesGamesID,
+    }
+}
+
+export default connect(mapStateToProps)(UserFavorites);
