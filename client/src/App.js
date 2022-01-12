b1ebd010ba3aa51a6f965d060c9dec42d7c4e0a1
Eric <eric_sanz_rodriguez@hotmail.com>
2022-01-12 15:55:06 +0100

    added 2 new routes to components and new icons

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import Landing from './components/Landing/Landing';
 import VideogamesList from './components/List/VideogamesList';
 import Details from './components/Details/Details';
 import Dashboard from './components/DashBoard/Dashboard';
+import UserFavorites from './components/DashBoard/UserFavorites/UserFavorites';
+import UsefulInformation from './components/DashBoard/UsefulInformation/UsefulInformation';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars, faSearch, faUser, faNewspaper, faShoppingCart, faSignInAlt, faCalendarAlt, faHome, faAngleDoubleRight, faAngleDoubleLeft, faAngleDoubleDown, faAngleDoubleUp, faEnvelopeOpenText, faHeart, faTimes, faCheckCircle, faEye, faEyeSlash, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faSearch, faUser, faNewspaper, faShoppingCart, faSignInAlt, faCalendarAlt, faHome, faAngleDoubleRight, faAngleDoubleLeft, faAngleDoubleDown, faAngleDoubleUp, faEnvelopeOpenText, faHeart, faTimes, faCheckCircle, faEye, faEyeSlash, faArrowRight, faExchangeAlt } from '@fortawesome/free-solid-svg-icons';
 import Login from './components/LogIn/Login';
 import Register from './components/Register/Register';
 
-library.add(faBars, faSearch, faUser, faNewspaper, faShoppingCart, faSignInAlt, faCalendarAlt, faHome, faAngleDoubleRight, faAngleDoubleLeft, faAngleDoubleDown, faAngleDoubleUp, faEnvelopeOpenText, faHeart, faTimes, faCheckCircle, faEye, faEyeSlash, faArrowRight)
+library.add(faBars, faSearch, faUser, faNewspaper, faShoppingCart, faSignInAlt, faCalendarAlt, faHome, faAngleDoubleRight, faAngleDoubleLeft, faAngleDoubleDown, faAngleDoubleUp, faEnvelopeOpenText, faHeart, faTimes, faCheckCircle, faEye, faEyeSlash, faArrowRight, faExchangeAlt)
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
         <Route path="/all-games" component={VideogamesList} />
         <Route path="/product/:videogameId" component={Details} />
         <Route path="/dashboard/:userId" exact component={Dashboard} />
+        <Route path="/dashboard/:userId/favorites" exact component={UserFavorites} />
+        <Route path="/dashboard/:userId/useful-information" exact component={UsefulInformation} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
       </Switch>
