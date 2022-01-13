184fbfb867c818d7a758e6e542570fcba96c938c
Eric <eric_sanz_rodriguez@hotmail.com>
2022-01-13 20:28:20 +0100

    fixed bug and added more options

--- client/src/components/DashBoard/ProfilePic/ProfilePic.jsx
+++ client/src/components/DashBoard/ProfilePic/ProfilePic.jsx
@@ -17,24 +17,51 @@ function ProfilePic({profilePicOptions, profilePictureOptions, setProfilePicture
         {
             name: 'Kratos',
             url: 'https://i.ibb.co/LQQLsHf/profile-kratos.png'
+        },
+        {
+            name: 'Cloud Strife',
+            url: 'https://i.ibb.co/nMYVTkB/profile-Cloud-Strife.png'
+        },
+        {
+            name: 'Ellie',
+            url: 'https://i.ibb.co/v4fbsz6/profile-ellie-tlou.png'
+        },
+        {
+            name: 'Samus Aran',
+            url: 'https://i.ibb.co/HKCBvcG/profile-samus-aran.png'
+        },
+        {
+            name: 'Crash Bandicoot',
+            url: 'https://i.ibb.co/SxxDsBC/profile-crash-bandicoot.png'
+        },
+        {
+            name: 'Link',
+            url: 'https://i.ibb.co/crtm8rn/profile-link.png'
+        },
+        {
+            name: 'Mario',
+            url: 'https://i.ibb.co/84GLmPv/profile-mario.png'
         }
     ]
 
     const saveButton = document.getElementById('save__btn--id');
     const closeOptions = document.getElementById('close__options--id');
-    const options = document.querySelectorAll('.options--id');
     const userId = user?.uid;
     
     saveButton?.addEventListener('click', () => {
+        let options = document.querySelectorAll('.options--id');
         for (let i = 0; i < options.length; i++) {
             if (options[i].checked) {
-                const photoUrl = options[i].value;
+                let photoUrl = options[i].value;
                 dispatch(changeProfilePic(userId, photoUrl));
+                if (user) {
+                }
             }
         }
+        dispatch(getUser(userId));
+
         profilePicOptions.style.display = 'none';
-        setProfilePictureOptions(!profilePictureOptions)
-        dispatch(getUser(user?.uid))
+        setProfilePictureOptions(!profilePictureOptions);
     })
 
     closeOptions?.addEventListener('click', () => {
@@ -49,10 +76,10 @@ function ProfilePic({profilePicOptions, profilePictureOptions, setProfilePicture
             <button name='options' className='save__btn' id='save__btn--id'>Save</button>
             <div className='pic__option--container'>
                 {picOptionsUrls.map((picOption) => (
-                    <div className='pic__option'>
+                    <div className='pic__option' key={picOption.name}>
                         <img src={picOption.url} alt={picOption.name} className='option__image'/>
                         <div className='input--container'>
-                            <input className='checkbox__option options--id' type="radio" value={picOption.url} name='options'/>
+                            <input className={picOption.url === user?.photoURL ? 'checkbox__option checked options--id' : 'checkbox__option options--id'} type="radio" value={picOption.url} name='options'/>
                             <label htmlFor={picOption.name} className='option__name'>{picOption.name}</label>
                         </div>
                     </div>
